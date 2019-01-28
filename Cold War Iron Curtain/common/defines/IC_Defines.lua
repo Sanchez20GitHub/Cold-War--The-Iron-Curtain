b25364d394224499d2ec5436a7fcf5f9ea00d304
Star Foth <40373723+starfoth@users.noreply.github.com>
2019-01-27 22:08:46 -0700

    Fixed econ 2.0 infra almost
    almost fixed

--- Cold War Iron Curtain/common/defines/IC_Defines.lua
+++ Cold War Iron Curtain/common/defines/IC_Defines.lua
@@ -17,7 +17,7 @@ NDefines.NAir.STRATEGIC_BOMBER_NUKE_AIR_SUPERIORITY = 0.0 -- How much air superi
 
 NDefines.NBuildings.AIRBASE_CAPACITY_MULT = 80 -- Each level of airbase building multiplied by this, gives capacity (max operational value). Value is int. 1 for each airplane.
 NDefines.NBuildings.ANTI_AIR_SUPERIORITY_MULT = 100.0 -- How much air superiority reduction to the enemy does our AA guns? Normally each building level = -1 reduction. With this multiplier.
-NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS= 0.2
+NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS= 0
 NDefines.NBuildings.MAX_BUILDING_LEVELS = 200
 NDefines.NBuildings.MAX_SHARED_SLOTS = 40
 NDefines.NBuildings.RADAR_RANGE_MAX = 220 -- Range is interpolated between building levels 1-15.
